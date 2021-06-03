79f50cbbd139184bcd163576d61a65e3f2f66dfd
Mad Max <salikman@ukr.net>
2021-06-03 19:43:43 +0300

    add page catalog

--- js/main.js
+++ js/main.js
@@ -1,53 +1,52 @@
 "use strict";
 
-(function($) {
-    $( ".navbar__lang--dropdown li a" ).each(function( index ) {
-		$( this ).on('click', () => {
-			$('.navbar__lang--current span').text($( this ).text())
-		})
-	});
-
-	$('.navbar__collapse').click(() => {
-        $('#nav').toggleClass('open-menu')
+(function ($) {
+  $(".navbar__lang--dropdown li a").each(function (index) {
+    $(this).on('click', () => {
+      $('.navbar__lang--current span').text($(this).text())
     })
+  });
 
-	$('.js-slider').slick({
-		slidesToShow: 1,
-        slidesToScroll: 1,
-        arrows: false,
-        dots: true,
-        dotsClass: 'dots',
-        fade: true,
-        speed: 500,
-        infinite: true,
-        cssEase: 'ease-in-out',
-        touchThreshold: 100,
-		adaptiveHeight: true
-	})
+  $('.navbar__collapse').click(() => {
+    $('#nav').toggleClass('open-menu')
+  })
+
+  $('.js-slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: true,
+    dotsClass: 'dots',
+    fade: true,
+    speed: 500,
+    infinite: true,
+    cssEase: 'ease-in-out',
+    touchThreshold: 100,
+    adaptiveHeight: true
+  })
 
   $('.js-gallery').slick({
-      infinite: false,
-      slidesToShow: 5,
-      slidesToScroll: 1,
-      arrows: false,
-      autoplay: true,
-      autoplaySpeed: 2000,
-      responsive: [
-        {
-          breakpoint: 1210,
-          settings: {
-            slidesToShow: 4,
-            
-          }
-        },
-        {
-          breakpoint: 1024,
-          settings: {
-            slidesToShow: 3,
-            
-          }
+    infinite: false,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [{
+        breakpoint: 1210,
+        settings: {
+          slidesToShow: 4,
+
         }
-      ]
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+
+        }
+      }
+    ]
   })
 
   // Init fancybox
@@ -96,15 +95,13 @@
 
       arrows: false,
       infinite: false,
-      responsive: [
-        {
-          breakpoint: 480,
-          settings: {
-            slidesToShow: 3,
-            slidesToScroll: 3,
-          },
+      responsive: [{
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
         },
-      ],
+      }, ],
     });
 
   $(".slider-single").on("afterChange", function (event, slick, currentSlide) {
@@ -123,9 +120,35 @@
   });
 
 
-  $('ul.tabs__caption').on('click', 'li:not(.active)', function() {
+  $('ul.tabs__caption').on('click', 'li:not(.active)', function () {
     $(this)
       .addClass('active').siblings().removeClass('active')
       .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+  });
+
+  $(function () {
+    // mileage volume
+    $("#mileage-range").slider({
+      range: true,
+      min: 0,
+      max: 100000,
+      values: [11000, 34000],
+      slide: function (event, ui) {
+        $("#mileageOne").val(ui.values[0]);
+        $("#mileageTwo").val(ui.values[1]);
+      }
+    });
+    $("#mileageOne").val($("#mileage-range").slider("values", 0));
+    $("#mileageTwo").val($("#mileage-range").slider("values", 1));
+
+  });
+
+  $('.btn-aside').on('click', function (e) {
+    e.preventDefault();
+    $('body').toggleClass('mobile-filter-open');
+    $('.shop-aside .close').on('click', function (e) {
+      e.preventDefault();
+      $('body').removeClass('mobile-filter-open');
+    });
   });
 })(jQuery)
